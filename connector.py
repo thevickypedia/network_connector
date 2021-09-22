731f96e1314f01cc51e7e36105f45877d1c6b5f8
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-22 16:44:03 -0500

    Add logging information Load env vars in .env file Write ssid information into a JSON file Update requirements.txt

--- connector.py
+++ connector.py
@@ -1,23 +1,53 @@
-from json import dumps
-from os import environ
+import logging
+from importlib import reload
+from json import dump
+from os import environ, path
 from re import sub
 from subprocess import PIPE, Popen
 
 import objc
+from dotenv import load_dotenv
+
+reload(logging)
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - [%(module)s:%(lineno)d] - %(funcName)s - %(message)s',
+    datefmt='%b-%d-%Y %I:%M:%S %p'
+)
+
+if path.isfile('.env'):
+    load_dotenv(dotenv_path='.env', verbose=True, override=True)
+
+logger = logging.getLogger(__name__)
 
 
-def get_ssid() -> str:
+def get_ssid() -> str or None:
+    """Gets SSID of the network connected.
+
+    Returns:
+        str or None:
+        WiFi or Ethernet SSID.
+    """
     process = Popen(
         ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'],
         stdout=PIPE)
     out, err = process.communicate()
     if error := process.returncode:
-        exit(f"Failed to fetch SSID with exit code: {error}\n{err}")
+        logger.error(f"Failed to fetch SSID with exit code: {error}\n{err}")
+        return
     # noinspection PyTypeChecker
     return dict(map(str.strip, info.split(': ')) for info in out.decode('utf-8').split('\n')[:-1]).get('SSID')
 
 
-def class_to_string_to_list_to_dict(network):
+def class_to_string_to_list_to_dict(network: classmethod) -> dict:
+    """Converts the class ``objective-c class CWNetwork`` to a dictionary object.
+
+    Args:
+        network: Takes the class object type ``objective-c`` as an argument.
+
+    Returns:
+        dict:
+        A dictionary object of information about the SSID.
+    """
     network_info = sub(r'<CWNetwork: \w+> ', '', str(network)).replace('[', '').replace(']', '')
     ssid_info = {}
     for element in network_info.split(', '):
@@ -26,7 +56,14 @@ def class_to_string_to_list_to_dict(network):
     return ssid_info
 
 
-def connector(ssid, password):
+def connector(ssid: str, password: str) -> None:
+    """Connects to the given SSID for WiFi connection.
+
+    Args:
+        ssid: Wifi access point name.
+        password: Password to connect to the SSID.
+    """
+    logger.info(f'Scanning for {ssid} in WiFi range')
     # noinspection PyUnresolvedReferences
     objc.loadBundle('CoreWLAN',
                     bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
@@ -36,8 +73,8 @@ def connector(ssid, password):
     interface = CWInterface.interface()
     networks, error = interface.scanForNetworksWithName_error_(ssid, None)
     if not networks:
-        print(f'Failed to detect the SSID: {ssid}')
-        print(error) if error else None
+        logger.error(f'Failed to detect the SSID: {ssid}')
+        logger.error(error) if error else None
         return
 
     network = networks.anyObject()
@@ -45,14 +82,15 @@ def connector(ssid, password):
     value = sub(r'<CWChannel: \w+> ', '', str(extracted.get('channel'))).split('=')
     extracted.pop('channel')
     extracted.update({value[0]: value[1]})
-    print(dumps(extracted, indent=2))
+    with open(f'{ssid}.json', 'w') as file:
+        dump(extracted, file, indent=2)
 
     success, error = interface.associateToNetwork_password_error_(network, password, None)
     if success:
-        print(f'Connected to {get_ssid()}')
-    if error:
-        print(f'Unable to connect to {ssid}')
-        print(error)
+        logger.info(f'Connected to {get_ssid() or ssid}')
+    elif error:
+        logger.error(f'Unable to connect to {ssid}')
+        logger.error(error)
 
 
 if __name__ == '__main__':
