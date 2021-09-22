90a90b38217ebeecca444d3174ee086e28f3d3b1
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-22 15:57:58 -0500

    Extract ssid information as a diction from objective-c class

--- connector.py
+++ connector.py
@@ -1,3 +1,7 @@
+from json import dumps
+from os import environ
+from re import sub
+
 import objc
 
 # noinspection PyUnresolvedReferences
@@ -7,6 +11,15 @@ objc.loadBundle('CoreWLAN',
 
 # noinspection PyUnresolvedReferences
 interface = CWInterface.interface()
-networks, error = interface.scanForNetworksWithName_error_(None, None)
-if networks:
-    print(networks)
+networks, error = interface.scanForNetworksWithName_error_(environ.get('ssid'), None)
+if not networks:
+    exit('No networks identified.')
+
+network = networks.anyObject()
+network_info = sub(r'<CWNetwork: \w+> ', '', str(network)).replace('[', '').replace(']', '')
+
+ssid_info = {}
+for element in network_info.split(', '):
+    dictionary = element.split('=', 1)
+    ssid_info.update({dictionary[0]: dictionary[1]})
+print(dumps(ssid_info, indent=2))
